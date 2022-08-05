80b7111ac4ac7522b15c3cf3b2c06e009e43d3aa
AlexWilliam <alexgoncalves.ti@gmail.com>
2022-08-04 21:45:50 -0300

    Adicionando projeto spring

---
+++ src/main/java/me/dio/SpringJavaDiOoApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaDiOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaDiOoApplication.class, args);
+	}
+
+}
